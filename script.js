1bff3c43dc17eebf64d2188d88da2a93544b644c
mlk <123862072+nikshitha293@users.noreply.github.com>
2024-08-11 15:14:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,47 @@ darkmode.onclick = () => {
 (function () {
     emailjs.init("QviDrXrwOnzj3Lf4j");
 })();
+
+function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+      }
+
+      function sendMail() {
+        var params = {
+          name: document.getElementById("name").value,
+          email: document.getElementById("email").value,
+          message: document.getElementById("message").value,
+        };
+      
+        if (!validateEmail(params.email)) {
+          var errorPopup = document.getElementById("errorPopup");
+          errorPopup.classList.add("show");
+          setTimeout(() => {
+            errorPopup.classList.remove("show");
+          }, 3000);
+          return;
+        }
+      
+        const serviceID = "service_xvjgevj";
+        const templateID = "template_h5ur1vp";
+      
+        emailjs.send(serviceID, templateID, params)
+        .then(res => {
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+      
+          // Show the success popup
+          var popup = document.getElementById("popup");
+          popup.classList.add("show");
+      
+          // Hide the popup after 3 seconds
+          setTimeout(() => {
+            popup.classList.remove("show");
+          }, 3000);
+      
+        })
+        .catch(err => console.log(err));
+      }
